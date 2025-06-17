52d8da58776eb5e396a40d4c1832a6cdc78f59fc
WZidane <wail.zidane@hotmail.com>
2025-06-17 18:11:08 +0000

    feat(kafka): added a second broker

--- flask/blueprints/news/__init__.py
+++ flask/blueprints/news/__init__.py
@@ -15,7 +15,7 @@ def lemonde_():
 
     consumer = KafkaConsumer(
         'raw-news',
-        bootstrap_servers='kafka:9092',
+        bootstrap_servers='10.11.31.193:9092',
         auto_offset_reset='earliest',
         enable_auto_commit=False,
         group_id='flask-viewer',
@@ -23,24 +23,72 @@ def lemonde_():
     )
 
     messages = []
-    for message in consumer:
-        data = message.value
 
-        # Formater la date si présente
-        if 'published' in data:
-            try:
-                dt = datetime.strptime(data['published'], "%a, %d %b %Y %H:%M:%S %Z")
-                # Formatage avec babel en français
-                data['published'] = format_datetime(dt, "d MMMM yyyy - HH:mm", locale='fr_FR')
-            except Exception as e:
-                print(f"⚠️ Erreur de parsing de date : {e}")
+    try: 
 
-        messages.append(data)
+        if message in consumer:
+            for message in consumer:
+                data = message.value
 
-        # On lit 10 messages et on s'arrête
-        if len(messages) >= 10:
-            break
+                # Formater la date si présente
+                if 'published' in data:
+                    try:
+                        dt = datetime.strptime(data['published'], "%a, %d %b %Y %H:%M:%S %Z")
+                        # Formatage avec babel en français
+                        data['published'] = format_datetime(dt, "d MMMM yyyy - HH:mm", locale='fr_FR')
+                    except Exception as e:
+                        print(f"⚠️ Erreur de parsing de date : {e}")
 
-    consumer.close()
+                messages.append(data)
+
+                # On lit 10 messages et on s'arrête
+                if len(messages) >= 10:
+                    break
+
+            consumer.close()
+    
+    except:
+        pass
 
     return render_template('lemonde.html', news_list=messages)
+
+@news.route('/news/20minutes')
+def vingtminutes_():
+    consumer = KafkaConsumer(
+        'raw-news',
+        bootstrap_servers='10.11.31.193:9092',
+        auto_offset_reset='earliest',
+        enable_auto_commit=False,
+        group_id='flask-viewer',
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
+
+    messages = []
+
+    try: 
+
+        if message in consumer:
+            for message in consumer:
+                data = message.value
+
+                # Formater la date si présente
+                if 'published' in data:
+                    try:
+                        dt = datetime.strptime(data['published'], "%a, %d %b %Y %H:%M:%S %Z")
+                        # Formatage avec babel en français
+                        data['published'] = format_datetime(dt, "d MMMM yyyy - HH:mm", locale='fr_FR')
+                    except Exception as e:
+                        print(f"⚠️ Erreur de parsing de date : {e}")
+
+                messages.append(data)
+
+                # On lit 10 messages et on s'arrête
+                if len(messages) >= 10:
+                    break
+
+            consumer.close()
+    
+    except:
+        pass
+
+    return render_template('vingtminutes.html', news_list=messages)
